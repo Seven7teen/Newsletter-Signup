fd440970f71c5a0f942a2950536fa0baa920a4ce
Seven7teen <aditprakash96@gmail.com>
2023-01-12 16:07:46 +0530

    api key visible

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post("https://newsletter-signup-tmjm.onrender.com", function(req,res) {
 
   const options = {
     method: "POST",
-    auth: "Aditya:"+process.env.API_KEY
+    auth: "Aditya:3360cc49910bde34a96e3329af291dbe-us8"
   }
 
   const request = https.request(url, options, function(response) {
